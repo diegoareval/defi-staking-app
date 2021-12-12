8bc90147750a93cdf7e023a3b2548fcc9e47c508
Diego <diego2000avelar@gmail.com>
2021-12-11 20:53:08 -0600

    setup dapp

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract Tether {
+
+    string public name = 'Tether';
+    string public symbol = 'USDT';
+    uint256 public totalSupply = 100000000;
+    uint8 public decimals = 18;
+
+}
